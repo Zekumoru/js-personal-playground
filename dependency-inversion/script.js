40d59a83746b9988e878b2550c1b29cbda601d4b
Zekumoru Dragonhart <dev@zekumoru.com>
2022-10-27 11:41:25 +0200

    Initial commit

---
+++ dependency-inversion/script.js
@@ -0,0 +1,63 @@
+/**
+ * Dependency Inversion Principle
+ * 
+ * A class should depend on an abstraction, i.e. wrapper class, than using directly its dependencies.
+ * 
+ * Video: https://www.youtube.com/watch?v=9oHY5TllWaU
+ * Check 3:15 in the video.
+ */
+
+class Store {
+  constructor(paymentProcessor) {
+    this.paymentProcessor = paymentProcessor;
+  }
+
+  purchaseBike(quantity) {
+    this.paymentProcessor.pay(200 * quantity);
+  }
+
+  purchaseHelmet(quantity) {
+    this.paymentProcessor.pay(15 * quantity);
+  }
+}
+
+class StripePaymentProcessor {
+  constructor(user) {
+    this.stripe = new Stripe(user);
+  }
+
+  pay(amountInDollars) {
+    this.stripe.makePayment(amountInDollars * 100);
+  }
+}
+
+class Stripe {
+  constructor(user) {
+    this.user = user;
+  }
+
+  makePayment(amountInCents) {
+    console.log(`${this.user} made payment of $${amountInCents / 100} with Stripe`);
+  }
+}
+
+class Paypal {
+  makePayment(user, amountInDollars) {
+    console.log(`${user} made payment of $${amountInDollars} with Paypal`);
+  }
+}
+
+class PaypalPaymentProcessor {
+  constructor(user) {
+    this.user = user;
+    this.paypal = new Paypal();
+  }
+
+  pay(amountInDollars) {
+    this.paypal.makePayment(this.user, amountInDollars);
+  }
+}
+
+const store = new Store(new StripePaymentProcessor('John'));
+store.purchaseBike(2);
+store.purchaseHelmet(2);
---
+++ interface-segregation/script.js
@@ -0,0 +1,72 @@
+/**
+ * Interface Segregation Principle
+ * 
+ * It means use every part of the interface.
+ * Implement all functions in an interface a class uses.
+ * 
+ * Video: https://www.youtube.com/watch?v=JVWZR23B_iE
+ */
+
+class Entity {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const mover = {
+  move() {
+    console.log(`${this.name} moved`);
+  }
+};
+
+const attacker = {
+  attack(targetEntity) {
+    console.log(`${this.name} attacked ${targetEntity.name} for ${this.attackDamage} damage`);
+    targetEntity.takeDamage(this.attackDamage);
+  }
+};
+
+const hasHealth = {
+  takeDamage(amount) {
+    this.health -= amount;
+    console.log(`${this.name} has ${this.health} health remaining`);
+  }
+};
+
+class Character extends Entity {
+  constructor(name, attackDamage, health) {
+    super(name);
+    this.attackDamage = attackDamage;
+    this.health = health;
+  }
+}
+
+Object.assign(Character.prototype, mover);
+Object.assign(Character.prototype, attacker);
+Object.assign(Character.prototype, hasHealth);
+
+class Wall extends Entity {
+  constructor(name, health) {
+    super(name);
+    this.health = health;
+  }
+}
+
+Object.assign(Wall.prototype, hasHealth);
+
+class Turret extends Entity {
+  constructor(name, attackDamage) {
+    super(name);
+    this.attackDamage = attackDamage;
+  }
+}
+
+Object.assign(Turret.prototype, attacker);
+
+const turret = new Turret('Turret', 5);
+const character = new Character('Character', 3 ,100);
+const wall = new Wall('Wall', 200);
+
+turret.attack(character);
+character.move();
+character.attack(wall);
