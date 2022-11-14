4d250772bd26af78e3290304c99a653c7d23b038
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2022-11-14 20:56:36 +0100

    Add button to fetch new cat image

--- giphy/script.js
+++ giphy/script.js
@@ -1,10 +1,30 @@
 const search = 'cats';
 const img = document.querySelector('img');
+const button = document.querySelector('button');
+let fetching = false;
 
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=${search}`, {
-  mode: 'cors',
-}).then((response) => {
-  return response.json();
-}).then((response) => {
-  img.src = response.data.images.original.url;
+button.addEventListener('click', () => {
+  setImage(search);
 });
+
+setImage(search);
+
+function setImage(search) {
+  fetching = true;
+  getGiphy(search, (url) => {
+    img.src = url;
+    fetching = false;
+  });
+}
+
+function getGiphy(search, urlFn) {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=${search}`, {
+    mode: 'cors',
+  }).then((response) => {
+    return response.json();
+  }).then((response) => {
+    if (typeof urlFn === 'function') urlFn(response.data.images.original.url);
+  }).catch(() => {
+    if (typeof urlFn === 'function') urlFn(null);
+  });
+}
