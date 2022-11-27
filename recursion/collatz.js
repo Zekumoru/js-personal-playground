05b0dc2bd4bd4f34fd5bee3cff84943e96cb759a
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2022-11-27 22:15:54 +0100

    Add collatz conjecture

---
+++ recursion/collatz.js
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=mz6tAJMVmfM
+// At 11:00
+//
+// Collatz conjecture
+//
+// If n is 1, stop.
+// Otherwise, if n is even, repeat this process on n / 2.
+// Otherwise, if n is odd, repeat this process on 3n + 1.
+//
+
+function collatz(n) {
+  console.log(n);
+  if (n === 1) return;
+  if (n % 2 === 0) return collatz(n / 2);
+  return collatz(3 * n + 1);
+}
+
+collatz(27);
