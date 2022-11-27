fe132ee16038b07c738c17d0e47d9318f9706f03
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2022-11-27 15:18:51 +0100

    Add task 5 linked list reverse loop

---
+++ recursion/task5.js
@@ -0,0 +1,42 @@
+// https://javascript.info/task/output-single-linked-list-reverse
+//
+// Output a single-linked list in the reverse order
+//
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+const loop = {
+  name: 'loop',
+  printListReverse(list) {
+    const output = [];
+
+    let current = list;
+    while (current) {
+      output.push(current.value);
+      current = current.next;
+    }
+
+    for (let i = output.length - 1; i >= 0; i--) {
+      console.log(output[i]);
+    }
+  }
+}
+
+function run(obj, list) {
+  console.log(obj.name);
+  obj.printListReverse(list);
+}
+
+run(loop, list);
