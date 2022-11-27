2b947e86f2207781e4989fd7444adcaeafe3e6a7
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-27 22:44:00 +0100

    Create Question class

--- recursion/quiz.js
+++ recursion/quiz.js
@@ -1,12 +1,24 @@
 // https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion
 
+class Question {
+  constructor(name, input, object = {}) {
+    this.name = name;
+    this.input = input;
+    Object.entries(object).forEach(([key, value]) => {
+      this[key] = value;
+    });
+  }
+
+  solve() {
+    throw new Error('Unimplemented solution');
+  }
+}
+
 // Question 1: Sum all numbers
 // Write a function called sumRange. 
 // It will take a number and return the sum of 
 // all numbers from 1 up to the number passed in.
-const q1 = {
-  name: 'Question 1: Sum all numbers',
-  input: 3,
+const q1 = new Question('Question 1: Sum all numbers', 3, {
   sumRange(n) {
     if (n == 0) return 0;
     return n + this.sumRange(n - 1);
@@ -14,7 +26,7 @@ const q1 = {
   solve() {
     return this.sumRange(this.input);
   }
-}
+});
 
 function run(question) {
   console.log(question.name);
