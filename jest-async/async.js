c486a3b35819e51bcb598e5d0fd758ea2a3eaa0e
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2022-12-07 15:34:46 +0100

    Add error and async

--- jest-async/async.js
+++ jest-async/async.js
@@ -1,3 +1,7 @@
 export async function fetchData() {
   return 'peanut butter';
 }
+
+export async function fetchDataError() {
+  throw new Error('error');
+}
--- jest-async/async.test.js
+++ jest-async/async.test.js
@@ -1,7 +1,16 @@
-import { fetchData } from './async';
+import { fetchData, fetchDataError } from './async';
 
 test('the data is peanut butter', () => {
   return fetchData().then(data => {
     expect(data).toBe('peanut butter');
   });
+});
+
+test('the data is peanut butter with async', async () => {
+  expect(await fetchData()).toBe('peanut butter');
+});
+
+test('throwing error', async () => {
+  expect.assertions(1);
+  return fetchDataError().catch((e) => expect(e.message).toMatch('error'));
 });
