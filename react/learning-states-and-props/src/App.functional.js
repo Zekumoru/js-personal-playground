77ddf198b2732a8f67d8e32ca52824ab35dfa1f4
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-01-02 16:18:45 +0100

    Create functional versions of App and MyComponent

---
+++ react/learning-states-and-props/src/App.functional.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyComponent from './components/MyComponent.functional';
+
+function App() {
+  return (
+    <div>
+      <MyComponent title="React" />
+    </div>
+  );
+}
+
+export default App;
