a3cba7e5ebb253ad90cad1353a5fb0eae958189f
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-02 15:38:02 +0100

    Add button to MyComponent

--- react/learning-states-and-props/src/App.js
+++ react/learning-states-and-props/src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClickBtn = this.onClickBtn.bind(this);
+  }
+
+  onClickBtn() {
+    console.log('Button has been clicked!');
+  }
+
   render() {
     return (
       <div>
-        <MyComponent title="React" />
+        <MyComponent title="React" onButtonClicked={this.onClickBtn} />
       </div>
     );
   }
--- react/learning-states-and-props/src/components/MyComponent.js
+++ react/learning-states-and-props/src/components/MyComponent.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class MyComponent extends React.Component {
   render() {
+    const { title, onButtonClicked } = this.props;
+
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{title}</h1>
+        <button onClick={onButtonClicked}>Click me!</button>
       </div>
     );
   }
