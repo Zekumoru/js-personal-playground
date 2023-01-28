3af4190d9bc95bd83f4235682f23cffd78bf1041
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-01-28 14:20:24 +0100

    Test function form of useState

--- react/use-state/src/App.js
+++ react/use-state/src/App.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react';
+
+const db = {
+  data: {},
+  get(key) {
+    console.log("I'm getting called!");
+    return this.data[key];
+  },
+  set(key, value) {
+    this.data[key] = value;
+  },
+};
+
+window.logData = () => {
+  console.log(db.data);
+};
+
 function App() {
-  return <div className="App"></div>;
+  const [input, setInput] = useState(() => {
+    return db.get('input') ?? '';
+  });
+
+  useEffect(() => {
+    db.set('input', input);
+  }, [input]);
+
+  return (
+    <div className="App">
+      <p>{input}</p>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
