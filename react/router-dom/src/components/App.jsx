a5e96fd8afae3b16715e210dffa1fbe34c25a093
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-01-30 23:52:41 +0100

    Add links to other routes

--- react/router-dom/src/components/App.jsx
+++ react/router-dom/src/components/App.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 const App = () => {
   return (
     <div className="App">
       <h1>Hello from App</h1>
+      <Link to="profile">Go to profile</Link>
     </div>
   );
 };
--- react/router-dom/src/components/Profile.jsx
+++ react/router-dom/src/components/Profile.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 const Profile = () => {
   return (
     <div>
       <h1>Hello from Profile</h1>
+      <Link to="/">Go back</Link>
     </div>
   );
 };
