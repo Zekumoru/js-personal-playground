045cb889e6a1b6b21a4795e4f4e62c29cd9f94bb
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-02-01 16:00:31 +0100

    Test react testing library

--- react/testing/src/App.js
+++ react/testing/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState } from 'react';
+
+const App = () => {
+  const [heading, setHeading] = useState('Magnificent Monkeys');
+  const clickHandler = () => setHeading('Radical Rhinos');
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App;
