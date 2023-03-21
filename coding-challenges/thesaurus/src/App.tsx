50f7ea413c90ac5f9f44f746bbe05f0c8a0ab4b7
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-03-21 15:09:44 +0100

    Finish thesaurus coding challenge

--- coding-challenges/thesaurus/src/App.tsx
+++ coding-challenges/thesaurus/src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+
+type Synonym = {
+  word: string;
+  score: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const [synonyms, setSynonyms] = useState<Synonym[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchSynonyms = async () => {
+    setLoading(true);
+
+    const response = await axios.get(
+      `https://api.datamuse.com/words?rel_syn=${input}`
+    );
+    const data = response.data;
+
+    setSynonyms(data as Synonym[]);
+    setLoading(false);
+  };
+
+  const handleClick = () => {
+    fetchSynonyms();
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={loading}
+      />
+      <button onClick={handleClick} disabled={loading}>
+        Find synonyms
+      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {synonyms.map((synonym) => (
+            <li key={synonym.word}>{synonym.word}</li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
