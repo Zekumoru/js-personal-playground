ea80fa2b0c4a78d63446135cad5cdeb7290917f1
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-03-22 02:57:50 +0100

    Add react query devtools

--- react-query/demo/src/main.tsx
+++ react-query/demo/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import './index.css';
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
 );
