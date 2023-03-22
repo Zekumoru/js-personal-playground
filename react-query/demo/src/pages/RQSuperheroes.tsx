5d526c1eca5646bfeb4657d2ee44c597c8f4271a
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-03-22 15:41:33 +0100

    Revert to fetching on mount

--- react-query/demo/src/pages/RQSuperheroes.tsx
+++ react-query/demo/src/pages/RQSuperheroes.tsx
@@ -5,11 +5,10 @@ import ErrorPreview from '../components/ErrorPreview';
 const RQSuperheroes = () => {
   const {
     data: superheroes,
+    isLoading,
     isError,
     error,
-    refetch,
-    isFetching,
-  } = useQuery('superheroes', fetchSuperheroes, { enabled: false });
+  } = useQuery('superheroes', fetchSuperheroes);
 
   if (isError) {
     return <ErrorPreview error={error} />;
@@ -17,19 +16,14 @@ const RQSuperheroes = () => {
 
   return (
     <div className="p-4">
-      <h2 className="mb-2 text-xl font-bold">RQ Super Heroes</h2>
-      <button className="btn mb-2" onClick={() => refetch()}>
-        Fetch Superheroes
-      </button>
-      {isFetching ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {superheroes?.map((hero) => (
-            <li key={hero.name}>{hero.name}</li>
-          ))}
-        </ul>
-      )}
+      <h2 className="mb-2 text-xl font-bold">
+        {isLoading ? 'Loading...' : 'RQ Super Heroes'}
+      </h2>
+      <ul>
+        {superheroes?.map((hero) => (
+          <li key={hero.name}>{hero.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
